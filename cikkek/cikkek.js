abe45a9b8f03b3ce2f8d067cca0dd3fd0b171fa5
mzsl <95681537+anomalili@users.noreply.github.com>
2022-04-26 12:04:40 +0200

    Update cikkek.js

--- cikkek/cikkek.js
+++ cikkek/cikkek.js
@@ -25,7 +25,7 @@ function CLASS(elem) {
 }
 
 
-
+const cikkek;
 
 function init () {
 
@@ -34,18 +34,19 @@ for (let index = 0; index < 2; index++) {
 
     cikkek = {
 
-        szerzo: "",
-        cim: "Az energiánk 20%-a szélerőművekből fog érkezni",
-        tema: 
-            {alcim: "", 
-            bekezdes: 
-                    [{"A szélenergia ipar legalább olyan gyorsan növekszik, mint a napelem ipar, ha nem gyorsabban.  Becslések szerint2030-ra globális szinten az energia 20%-a szélerőművekből fog származni."},
-                    {"A technológia ugyan drága, és sokkal nagyobb mértékű beruházásokra van szükség, mint a napelemes befektetéseknél, de a szélenergia hosszútávú előnyei sokkal kecsegtetőbbek. Ha az átállás sikeres lenne  az azt jelentené, hogy évente 3,6 milliárd tonnával kevesebb széndioxidot engednénk a levegőbe."},
-                    {"Kína az egyik legkiemelkedőbb vezető ország, ha szélenergiáról van szó. Tavaly 17%-ra növelték a szélenergiából befolyó energia mennyiségét az országban, és jelenleg is további bővítéseket terveznek, még idén. Ezen kívül már született megegyezés abban, hogy 1.000 szénbányát fognak bezárni országszerte, ezzel is elősegítve a megújuló energiaforrásokba történő befektetéseket." },
-                    {"Az jó ügy édekében Kínának és még sok más országnak teljesen át kell alakítania a hálózat infrastruktúráját, hogy teljesen ki tudják használni a megújuló energiaforrásokat."}
-    ]}      felsorolas:"", 
-            link: "https://www.tisztajovo.hu/megujulo-energiaforrasok/2016/10/19/az-energiank-20-a-szeleromuvekbol-fog-erkezni",
-            kep:""
+        "szerzo": "",
+        "cim": "Az energiánk 20%-a szélerőművekből fog érkezni",
+        "tema": 
+            {"alcim": " ", 
+            "bekezdes": 
+                    "A szélenergia ipar legalább olyan gyorsan növekszik, mint a napelem ipar, ha nem gyorsabban.  Becslések szerint 2030-ra globális szinten az energia 20%-a szélerőművekből fog származni.",
+                    "A technológia ugyan drága, és sokkal nagyobb mértékű beruházásokra van szükség, mint a napelemes befektetéseknél, de a szélenergia hosszútávú előnyei sokkal kecsegtetőbbek. Ha az átállás sikeres lenne  az azt jelentené, hogy évente 3,6 milliárd tonnával kevesebb széndioxidot engednénk a levegőbe.",
+                    "Kína az egyik legkiemelkedőbb vezető ország, ha szélenergiáról van szó. Tavaly 17%-ra növelték a szélenergiából befolyó energia mennyiségét az országban, és jelenleg is további bővítéseket terveznek, még idén. Ezen kívül már született megegyezés abban, hogy 1.000 szénbányát fognak bezárni országszerte, ezzel is elősegítve a megújuló energiaforrásokba történő befektetéseket.",
+                    "Az jó ügy édekében Kínának és még sok más országnak teljesen át kell alakítania a hálózat infrastruktúráját, hogy teljesen ki tudják használni a megújuló energiaforrásokat."}
+                }'      
+            "felsorolas": "", 
+            "link": "https://www.tisztajovo.hu/megujulo-energiaforrasok/2016/10/19/az-energiank-20-a-szeleromuvekbol-fog-erkezni",
+            "kep":""
        },{
 
         szerzo: "",
