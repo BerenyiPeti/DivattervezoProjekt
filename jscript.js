345eda85bad87d9b7b1b1151dd9f9ab6f4ded5b3
BerenyiPeti <berenyipeti01@gmail.com>
2022-04-26 08:45:17 +0200

    alam html + alap jscript

---
+++ jscript.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", init);
+
+function ID(elem) {
+    return document.getElementById(elem);
+}
+
+function CLASS(elem) {
+    return document.getElementsByClassName(elem);
+}
+
+function QS(elem) {
+    return document.querySelector(elem);
+}
+
+function QSA(elem) {
+    return document.querySelectorAll(elem);
+}
+
+function init() {
+    
+}
