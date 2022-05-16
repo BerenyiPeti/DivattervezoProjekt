bf0b8cb3d5e76ad0e0c6f9d1f3e8901b08756361
BerenyiPeti <berenyipeti01@gmail.com>
2022-05-16 21:30:46 +0200

    szelektív játék majdnem kész

--- jatekok/jscriptSzelektivJatek.js
+++ jatekok/jscriptSzelektivJatek.js
@@ -13,9 +13,55 @@ function QSA(elem) {
 }
 
 const szemetek = [];
+/*let kukak = ["../kepek/jatekok/feherkuka.png","../kepek/jatekok/sargakuka.png", "../kepek/jatekok/zoldkuka.png", "../kepek/jatekok/kekkuka.png"]*/
+let kukak = [
+    {
+        eleres: "../kepek/jatekok/feherkuka.png",
+        tipus: "feherkuka"
+    },
+    {
+        eleres: "../kepek/jatekok/sargakuka.png",
+        tipus: "sargakuka"
+    },
+    {
+        eleres: "../kepek/jatekok/zoldkuka.png",
+        tipus: "zoldkuka"
+
+    },
+    {
+        eleres: "../kepek/jatekok/kekkuka.png",
+        tipus: "kekkuka"
+    }
+]
+
+var pontszam = 0;
+var szemetIndex = 0;
+var ido = 10;
+
 
 function init() {
+    /*ido = 10;
+    ID("jatekKezdet").addEventListener("click", indit);*/
+    indit();
+}
+
+function indit() {
+
+    ID("kezdoKep").style.display = "none";
     jsonHivas();
+    /*kukak.forEach(kukak => {
+        kukak.addEventListener("click", () => {
+            console.log("kuka");
+        });
+    });*/
+    kukaKever();
+    ID("pontszam").innerHTML = pontszam;
+    ID("idozito").innerHTML = ido;
+    ID("ujJatek").addEventListener("click", init)
+
+    setInterval(idozito, 1000);
+
+
 }
 
 function jsonHivas() {
@@ -38,7 +84,60 @@ function beszur(tomb) {
 }
 
 function megjelenit() {
-    var szam = Math.random() * szemetek.length;
+    szemetIndex = Math.floor(Math.random() * szemetek.length);
     //console.log(szemetek[Math.floor(szam)].nev);
-    ID("szemetTarolo").innerHTML = `<img src="${szemetek[Math.floor(szam)].eleres}">`;
+    ID("szemetTarolo").innerHTML = `<img src="${szemetek[szemetIndex].eleres}">`;
 }
+
+function valaszt() {
+    //console.log("kuka");
+    let tipus = szemetek[szemetIndex].tipus;
+    if (tipus === event.target.alt) {
+        console.log("jó");
+        hatter("greenyellow")
+        pontszam++;
+    } else {
+
+        console.log("rossz");
+        hatter("red")
+        if (pontszam > 0) {
+            pontszam--;
+
+        }
+    }
+
+    ID("pontszam").innerHTML = pontszam;
+    megjelenit();
+    kukaKever();
+}
+
+function hatter(szin) {
+    ID("pontszamTarolo").style.background = szin
+
+}
+
+function kukaKever() {
+    let txt = "";
+    kukak = kukak.sort(() => Math.random() - 0.5);
+    for (let index = 0; index < kukak.length; index++) {
+        txt += `<img draggable="false" src="${kukak[index].eleres}" alt="${kukak[index].tipus}">`;
+
+    }
+
+    ID("kukaTarolo").innerHTML = txt;
+
+    for (let index = 0; index < 4; index++) {
+        document.querySelectorAll("#kukaTarolo img")[index].addEventListener("click", valaszt);
+
+    }
+}
+
+function idozito() {
+    if (ido > 0) {
+        ido -= 1;
+        ID("idozito").innerHTML = ido;
+    }
+
+}
+
+
